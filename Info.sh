61754a1724a7e34c2def4d2c261121a552834e10
DangoDivs <nwadikedivine28@gmail.com>
2026-09-09 18:03:53 +0100

    update Info.sh

--- Info.sh
+++ Info.sh
@@ -4,4 +4,8 @@ read -p "SecondName" second
 echo "$second"
 read -p "How old are you" age
 echo "$age"
+read -p "If you love food write Right If you don't write Left" Answer
+if [ "$Answer" -eq "Right"] || [ "$Answer" -eq "right"]; then
+echo "You love food"
+else echo "You don't love food"
 
