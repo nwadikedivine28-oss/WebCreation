4c9dfe50ea0d452fd0750a11d4709db0b23acedf
DangoDivs <nwadikedivine28@gmail.com>
2026-09-09 18:12:55 +0100

    update Info.sh

--- Info.sh
+++ Info.sh
@@ -1,8 +1,8 @@
 
 read -p "If you love food write Right If you don't write Left  " Answer
-if [ "$Answer" -eq "Right"] || [ "$Answer" -eq "right"]; then
+if [ "$Answer" = "Right"] || [ "$Answer" = "right"]; then
 echo "You love food"
-elif [ "$Answer" -eq "Left"] || [ "$Answer" -eq "left"]; then
+elif [ "$Answer" = "Left"] || [ "$Answer" = "left"]; then
 echo "You don't love food"
 else echo "you gave the wrong option"
 fi
